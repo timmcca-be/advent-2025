73ff5bce710f37cba2580860efc7f6206b0975dd
Tim McCabe <timmccabe@khanacademy.org>
2025-12-04 15:21:45 -0500

    support timing

--- day_3/main.py
+++ day_3/main.py
@@ -30,20 +30,28 @@ def solve_part_1(input_lines):
 def solve_part_2(input_lines):
     return solve(input_lines, 12)
 
+import time
 from pathlib import Path
 
 script_dir = Path(__file__).parent
 
+def run_and_time(part_name, func, lines):
+    start_time = time.time()
+    answer = func(lines)
+    end_time = time.time()
+    runtime = end_time - start_time
+    print(f"Part {part_name}: {answer} ({runtime:.3f} sec)")
+
 print()
 with open(script_dir / "input_example.txt", "r") as file:
     lines = [line.rstrip("\n") for line in file]
-    print("Part 1 (example):", solve_part_1(lines))
-    print("Part 2 (example):", solve_part_2(lines))
+    run_and_time("1 (example)", solve_part_1, lines)
+    run_and_time("2 (example)", solve_part_2, lines)
 
 print()
 with open(script_dir / "input_real.txt", "r") as file:
     lines = [line.rstrip("\n") for line in file]
-    print("Part 1 (real):", solve_part_1(lines))
-    print("Part 2 (real):", solve_part_2(lines))
+    run_and_time("1 (real)", solve_part_1, lines)
+    run_and_time("2 (real)", solve_part_2, lines)
 
 print()
