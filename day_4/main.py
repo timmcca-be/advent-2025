65f9b6a5964923ba6b9ccb9e11236783f7d73277
Tim McCabe <timmccabe@khanacademy.org>
2025-12-04 15:45:09 -0500

    solve day 4

---
+++ day_4/main.py
@@ -0,0 +1,72 @@
+def solve_part_1(input_lines):
+    locations = set()
+    for (y, line) in enumerate(input_lines):
+        for (x, character) in enumerate(line):
+            if character == "@":
+                locations.add((x, y))
+
+    result = 0
+    for x, y in locations:
+        neighbors = set(
+            (x + dx, y + dy)
+            for dx in [-1, 0, 1]
+            for dy in [-1, 0, 1]
+        ) - set([(x, y)])
+        neighboring_rolls = neighbors & locations
+        if len(neighboring_rolls) < 4:
+            result += 1
+
+    return result
+
+def solve_part_2(input_lines):
+    locations = set()
+    for (y, line) in enumerate(input_lines):
+        for (x, character) in enumerate(line):
+            if character == "@":
+                locations.add((x, y))
+
+    result = 0
+    while True:
+        new_locations = set()
+        for x, y in locations:
+            neighbors = set(
+                (x + dx, y + dy)
+                for dx in [-1, 0, 1]
+                for dy in [-1, 0, 1]
+            ) - set([(x, y)])
+            neighboring_rolls = neighbors & locations
+            if len(neighboring_rolls) < 4:
+                result += 1
+            else:
+                new_locations.add((x, y))
+        if len(new_locations) == len(locations):
+            break
+        locations = new_locations
+
+    return result
+
+import time
+from pathlib import Path
+
+script_dir = Path(__file__).parent
+
+def run_and_time(part_name, func, lines):
+    start_time = time.time()
+    answer = func(lines)
+    end_time = time.time()
+    runtime = end_time - start_time
+    print(f"Part {part_name}: {answer} ({runtime:.3f} sec)")
+
+print()
+with open(script_dir / "input_example.txt", "r") as file:
+    lines = [line.rstrip("\n") for line in file]
+    run_and_time("1 (example)", solve_part_1, lines)
+    run_and_time("2 (example)", solve_part_2, lines)
+
+print()
+with open(script_dir / "input_real.txt", "r") as file:
+    lines = [line.rstrip("\n") for line in file]
+    run_and_time("1 (real)", solve_part_1, lines)
+    run_and_time("2 (real)", solve_part_2, lines)
+
+print()
